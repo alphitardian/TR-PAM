7b028b97aeeecf3714ba2de40aea8cdb07313b4a
Ardian Alphita <ardianpramudya81@gmail.com>
2021-04-19 16:43:07 +0700

    Add Bottom Navigation Bar

--- app/src/main/java/com/alphitardian/tr_pam/MainActivity.java
+++ app/src/main/java/com/alphitardian/tr_pam/MainActivity.java
@@ -1,15 +1,22 @@
 package com.alphitardian.tr_pam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.alphitardian.tr_pam.api.ApiList;
 import com.alphitardian.tr_pam.api.RetrofitClient;
+import com.alphitardian.tr_pam.fragment.HomeFragment;
+import com.alphitardian.tr_pam.fragment.NewsFragment;
+import com.alphitardian.tr_pam.fragment.ProfileFragment;
 import com.alphitardian.tr_pam.model.CryptoData;
 import com.alphitardian.tr_pam.model.CryptoList;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -17,14 +24,34 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+
         getAllCrypto();
+    }
+
+    // Replace Fragment in the Host Fragment
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.host_fragment, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     private void getAllCrypto() {
@@ -47,5 +74,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                Toast.makeText(this, "Home View", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_profile:
+                fragment = new ProfileFragment();
+                Toast.makeText(this, "Profile VIew", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_news:
+                fragment = new NewsFragment();
+                Toast.makeText(this, "News View", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
---
+++ app/src/main/java/com/alphitardian/tr_pam/fragment/NewsFragment.java
@@ -0,0 +1,34 @@
+package com.alphitardian.tr_pam.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.alphitardian.tr_pam.R;
+
+public class NewsFragment extends Fragment {
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textView = view.findViewById(R.id.news_textview);
+
+        textView.setText("News Fragment");
+    }
+}
