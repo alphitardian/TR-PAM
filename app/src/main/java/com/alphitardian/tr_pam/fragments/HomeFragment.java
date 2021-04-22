f004382c207e3b290ea470ebc8c49bdc657c06a3
Kelvin Kurniawan O <tepinnko@gmail.com>
2021-04-23 00:26:35 +0700

    Refine main page

--- app/src/main/java/com/alphitardian/tr_pam/fragments/HomeFragment.java
+++ app/src/main/java/com/alphitardian/tr_pam/fragments/HomeFragment.java
@@ -8,16 +8,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.alphitardian.tr_pam.R;
+import com.alphitardian.tr_pam.apis.ApiList;
+import com.alphitardian.tr_pam.apis.RetrofitClient;
+import com.alphitardian.tr_pam.models.CurrentBalance;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
-    private TextView textView;
+    private TextView homeBalanceValue, homeBuyValue, homeSellValue;
 
     SharedPreferences pref;
 
@@ -34,7 +45,41 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        homeBalanceValue = getActivity().findViewById(R.id.homeBalanceValue);
+        homeBuyValue = getActivity().findViewById(R.id.homeBuyValue);
+        homeSellValue = getActivity().findViewById(R.id.homeSellValue);
+        getCurrentBalance();
+    }
 
-        String fullName = pref.getString("fullName", "full name");
+    public void getCurrentBalance(){
+        ApiList apiList = RetrofitClient.getRetrofitClient().create(ApiList.class);
+        Call<CurrentBalance> call = apiList.getCurrentBalance(pref.getString("userId", ""));
+
+
+        call.enqueue(new Callback<CurrentBalance>() {
+            @Override
+            public void onResponse(Call<CurrentBalance> call, Response<CurrentBalance> response) {
+                if(response.isSuccessful()){
+                    CurrentBalance currentBalance = response.body();
+                    String balance = currentBalance.getCurrent();
+                    Double Buy = currentBalance.getBalanceTransaction().getTransactionBuy();
+                    Double Sell = currentBalance.getBalanceTransaction().getTransactionSell();
+
+                    NumberFormat format = NumberFormat.getCurrencyInstance();
+                    format.setMaximumFractionDigits(0);
+
+                    format.setCurrency(Currency.getInstance("USD"));
+
+                    homeBalanceValue.setText(format.format(Double.parseDouble(balance)));
+                    homeBuyValue.setText(format.format(Buy));
+                    homeSellValue.setText(format.format(Sell));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CurrentBalance> call, Throwable t) {
+                Log.w("error wallet", t.toString());
+            }
+        });
     }
 }
