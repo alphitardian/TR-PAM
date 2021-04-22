2d7b7a090551ed68d12f6b9c1ca0f9dcc9eecc83
Ardian Alphita <ardianpramudya81@gmail.com>
2021-04-22 22:04:56 +0700

    Add Image Picker for Profile Picture

--- app/src/main/java/com/alphitardian/tr_pam/EditProfileActivity.java
+++ app/src/main/java/com/alphitardian/tr_pam/EditProfileActivity.java
@@ -3,31 +3,36 @@ package com.alphitardian.tr_pam;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alphitardian.tr_pam.models.UserDetail;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static com.alphitardian.tr_pam.utils.PathProvider.getPath;
+
 public class EditProfileActivity extends AppCompatActivity {
 
     private final int MAPACTIVITY_REQ_CODE = 1;
-    private static String EXTRA_ADDRESS = "extra_address";
+    private final int GALLERY_IMAGE_REQ_CODE = 4;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -37,9 +42,11 @@ public class EditProfileActivity extends AppCompatActivity {
     SharedPreferences.Editor editor;
 
     EditText fName, uName, email, password, address, confirmPassword;
-    String _fName, _uName, _email, _password, _address, _confirmPassword;
+    String _fName, _uName, _email, _password, _address, _confirmPassword, _photoPath;
     Button _btnSaveProfile;
-    ImageView _btnLocation;
+    ImageView _btnLocation, _profileImage;
+
+    private Bitmap photoBitmap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +61,7 @@ public class EditProfileActivity extends AppCompatActivity {
         address = findViewById(R.id.newEditTextAddress);
         _btnSaveProfile = findViewById(R.id.btnSaveProfile);
         _btnLocation = findViewById(R.id.location_button);
+        _profileImage = findViewById(R.id.profile_image);
 
         preferences = this.getSharedPreferences("USER_DATA", Context.MODE_PRIVATE);
         editor = preferences.edit();
@@ -62,6 +70,11 @@ public class EditProfileActivity extends AppCompatActivity {
         uName.setText(preferences.getString("username", ""));
         email.setText(preferences.getString("email", ""));
         address.setText(preferences.getString("address", ""));
+        _profileImage.setImageBitmap(photoBitmap);
+        Glide.with(getApplicationContext())
+                .load(preferences.getString("photo_path", ""))
+                .override(200,200)
+                .into(_profileImage);
 
         _btnSaveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,9 +90,18 @@ public class EditProfileActivity extends AppCompatActivity {
                 startActivityForResult(intent, MAPACTIVITY_REQ_CODE);
             }
         });
+
+        _profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_IMAGE_REQ_CODE);
+            }
+        });
     }
 
-    public void saveEditProfile(){
+    public void saveEditProfile() {
 
         _fName = fName.getText().toString();
         _uName = uName.getText().toString();
@@ -88,31 +110,32 @@ public class EditProfileActivity extends AppCompatActivity {
         _confirmPassword = password.getText().toString();
         _address = address.getText().toString();
 
-        if(TextUtils.isEmpty(_fName) || TextUtils.isEmpty(_uName) || TextUtils.isEmpty(_password) || TextUtils.isEmpty(_address) || TextUtils.isEmpty(_email)){
+        if (TextUtils.isEmpty(_fName) || TextUtils.isEmpty(_uName) || TextUtils.isEmpty(_password) || TextUtils.isEmpty(_address) || TextUtils.isEmpty(_email)) {
             Toast.makeText(EditProfileActivity.this, "Fill the blank form!",
                     Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             editor.putString("fullName", _fName);
             editor.putString("username", _uName);
             editor.putString("email", _email);
             editor.putString("address", _address);
+            editor.putString("photo_path", _photoPath);
             editor.apply();
 
-            saveProfile(_email, _password, _fName, _uName, _address);
+            saveProfile(_email, _password, _fName, _uName, _address, _photoPath);
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             setResult(RESULT_OK, intent);
             finish();
         }
     }
 
-    public void saveProfile(String email, String password, String fullName, String userName, String address) {
+    public void saveProfile(String email, String password, String fullName, String userName, String address, String photoPath) {
 
         if (!_confirmPassword.equals(password)) {
             Toast.makeText(EditProfileActivity.this, "Password is not equal", Toast.LENGTH_LONG);
         } else {
             user.updateEmail(email);
             user.updatePassword(password);
-            UserDetail userDetail = new UserDetail(userName, fullName, address, "default");
+            UserDetail userDetail = new UserDetail(userName, fullName, address, photoPath);
             String uid = user.getUid();
             db.collection("users").document(uid).set(userDetail);
         }
@@ -125,6 +148,17 @@ public class EditProfileActivity extends AppCompatActivity {
         if (requestCode == MAPACTIVITY_REQ_CODE) {
             if (resultCode == RESULT_OK) {
                 address.setText(data.getStringExtra(MapsActivity.EXTRA_ADDRESS));
+            }
+        }
+
+        if (requestCode == GALLERY_IMAGE_REQ_CODE) {
+            if (resultCode == RESULT_OK) {
+                Glide.with(getApplicationContext())
+                        .load(getPath(getApplicationContext(), data.getData()))
+                        .override(200,200)
+                        .into(_profileImage);
+                _photoPath = getPath(getApplicationContext(), data.getData());
+                Log.d("IMAGE_PICKER", "onActivityResult: " + _photoPath);
             }
         }
     }
--- app/src/main/java/com/alphitardian/tr_pam/fragments/ProfileFragment.java
+++ app/src/main/java/com/alphitardian/tr_pam/fragments/ProfileFragment.java
@@ -1,38 +1,31 @@
 package com.alphitardian.tr_pam.fragments;
 
-import android.content.Intent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.os.Handler;
-import android.os.Looper;
-import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.alphitardian.tr_pam.EditProfileActivity;
-import com.alphitardian.tr_pam.MapsActivity;
 import com.alphitardian.tr_pam.R;
-
-import com.alphitardian.tr_pam.RegisterActivity;
-
 import com.alphitardian.tr_pam.WalletActivity;
+import com.bumptech.glide.Glide;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.IOException;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -42,6 +35,7 @@ public class ProfileFragment extends Fragment {
 
     private TextView fullNameTextView, usernameTextView, emailTextView, addressTextView;
     private ImageView profileImage, editButton, walletButton;
+    private Bitmap photoBitmap;
 
     SharedPreferences pref;
 
@@ -70,6 +64,10 @@ public class ProfileFragment extends Fragment {
         usernameTextView.setText(pref.getString("username", "Username"));
         emailTextView.setText(pref.getString("email", "Email"));
         addressTextView.setText(pref.getString("address", "Address"));
+        Glide.with(getContext())
+                .load(pref.getString("photo_path", ""))
+                .override(200,200)
+                .into(profileImage);
 
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,6 +95,10 @@ public class ProfileFragment extends Fragment {
                 usernameTextView.setText(pref.getString("username", "Username"));
                 emailTextView.setText(pref.getString("email", "Email"));
                 addressTextView.setText(pref.getString("address", "Address"));
+                Glide.with(getContext())
+                        .load(pref.getString("photo_path", ""))
+                        .override(200,200)
+                        .into(profileImage);
             }
         }
     }
