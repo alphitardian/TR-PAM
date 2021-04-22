e5bbee1183fc00a685be0a2c20ada1a7da6fbd37
cnugroho11 <cnugroho211@gmail.com>
2021-04-22 18:31:58 +0700

    ui buy crypto

--- app/src/main/java/com/alphitardian/tr_pam/BuyCryptoActivity.java
+++ app/src/main/java/com/alphitardian/tr_pam/BuyCryptoActivity.java
@@ -2,14 +2,30 @@ package com.alphitardian.tr_pam;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BuyCryptoActivity extends AppCompatActivity {
+
+    private TextView txtViewCrypto, priceTxtView;
+    private EditText editTextQuantity;
+    private ImageView btnDecrease, btnIncrease;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buy_crypto_screen);
+
+        txtViewCrypto = findViewById(R.id.txtViewCrypto);
+        priceTxtView = findViewById(R.id.txtViewPrice);
+        editTextQuantity = findViewById(R.id.editTextQuantity);
+        btnIncrease = findViewById(R.id.btnIncrease);
+        btnDecrease = findViewById(R.id.btnDecrease);
+
     }
 }
